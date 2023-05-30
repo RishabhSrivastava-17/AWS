44db202dfb3648cf869c7edca266ee84f81ad885
Rishabh Srivastava <72572136+RishabhSrivastava-17@users.noreply.github.com>
2023-05-30 11:20:25 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-2.cl6oejadnsx0.eu-north-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "rishabhsri"
+customdb = "employee"
+custombucket = "addthebucket"
+customregion = "eu-north-1"
